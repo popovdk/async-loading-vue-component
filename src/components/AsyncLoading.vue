7aaa8c8712cd14e19aeeeec4b0da9b2b6a0e858d
Danil <popovdk01@gmail.com>
2021-01-26 13:42:42 +0400

    Add broadcast data attrs and props

--- src/components/AsyncLoading.vue
+++ src/components/AsyncLoading.vue
@@ -69,9 +69,26 @@ export default {
             // Собираем чанк в компонент
             const Component = Vue.extend(chunk.default)
 
+            // Собираем обект пропсов
+            let propsData = {}
+            if (this.asyncComponent.data.attrs) {
+              propsData = Object.entries(this.asyncComponent.data.attrs)
+              propsData = propsData.map(([key, value]) => {
+                const upperSpl = key.split('-').map((item, index) => {
+                  if (index > 0) {
+                    return item.charAt(0).toUpperCase() + item.substr(1).toLowerCase()
+                  }
+                  return item
+                }).join('')
+                return [upperSpl, value]
+              })
+
+              propsData = Object.fromEntries(propsData)
+            }
+
             // Создаем компонент
             const component = new Component({
-              propsData: this.asyncComponent.data.attrs
+              propsData
             })
 
             // Пробрасываем слоты
@@ -86,8 +103,10 @@ export default {
             component.$slots.default = this.asyncComponent.componentOptions.children
 
             // Вешаем события
-            for (const [event, handler] of Object.entries(this.asyncComponent.componentOptions.listeners)) {
-              component.$on(event, handler)
+            if (this.asyncComponent.componentOptions.listeners) {
+              for (const [event, handler] of Object.entries(this.asyncComponent.componentOptions.listeners)) {
+                component.$on(event, handler)
+              }
             }
 
             // Собираем html
@@ -95,6 +114,11 @@ export default {
 
             // Вешаем css классы
             component.$el.classList.add(...this.asyncComponent.data.staticClass.split(' '))
+
+            // Переносим data аттрибуты
+            for (const attr in this.$el.dataset) {
+              component.$el.dataset[attr] = ''
+            }
 
             // Добавляем компонент в DOM вместо якоря
             this.$el.outerHTML = component.$el.outerHTML
@@ -104,10 +128,12 @@ export default {
           .catch(e => console.error(e.message))
       }
     }
+
     const options = {
       threshold: this.threshold,
       rootMargin: this.rootMargin
     }
+
     this.observer = new IntersectionObserver(handler, options)
 
     // Нужно подождать следующего тика для того, чтобы дочерний элемент мог отрисоваться.
